a482c0158ef3d392cc2cbe8f56d9f50552c3e167
DCI13 <163683386+DCI13@users.noreply.github.com>
2024-03-23 18:31:58 +0100

    Update portada.js

--- src/portada.js
+++ src/portada.js
@@ -23,9 +23,11 @@ function Portada() {
         </NavLink>
         
         <NavLink to="/registro_vendedor">
-            <button>Vendedor</button>
+            <button type="submit">Vendedor</button>
         </NavLink>
-        <button>Voluntario</button>
+        <NavLink to="/registro_voluntario">
+            <button type="submit">Voluntario</button>
+        </NavLink>
         </div>
     </div>
   );
