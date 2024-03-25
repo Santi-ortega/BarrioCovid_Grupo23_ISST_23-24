ea143d1226adcc02d65d7963438a34f051bf788c
Alvaro Santorum <a.santorum@alumnos.upm.es>
2024-03-25 19:01:14 +0100

    Actualizacion Registro vendedor y voluntario

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Portada from './portada';
 import Registro_Comprador from "./registro_comprador";
+import Registro_Vendedor from "./registro_vendedor";
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Portada />} />
             <Route path="/registro_comprador" element={<Registro_Comprador />} />
+            <Route path="/registro_vendedor" element={<Registro_Vendedor />} />
           </Routes>
         </BrowserRouter>
       
