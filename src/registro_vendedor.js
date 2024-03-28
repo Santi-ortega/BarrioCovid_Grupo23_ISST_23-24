ba2391207682a3697a243cc97ac968544f85eb96
santi-ortega <santiof.12@gmail.com>
2024-03-28 10:39:44 +0100

    Inicio implementación lógica registro vendedor

--- src/registro_vendedor.js
+++ src/registro_vendedor.js
@@ -6,49 +6,54 @@ import BackButton from './BackButton';
 import SubirFoto from './SubirFoto';
 
 function Registro_Vendedor() {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [contraseña, setContraseña] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [tienda, setTienda] = useState('');
+  const [horario, setHorario] = useState('');
+  const [imagen, setImagen] = useState('');
     return (
         <div>
           <h2>Registro de Vendedor</h2>
           <form className='form'>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Nombre</label>
-              <input type="text"/>
+              <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Apellidos</label>
-              <input type="text"/>
+              <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Teléfono</label>
-              <input type="text" />
+              <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Correo</label>
-              <input type="text" />
+              <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)}/>
             </div>            
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Contraseña</label>
-              <input type="password" />
-            </div>
-            <div className='apartado'>
-              <label className='parametro_registro_comprador'>Repetir Contraseña</label>
-              <input type="password"/>
+              <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Dirección</label>
-              <input type="text"/>
+              <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Tienda</label>
-              <input type="text"/>
+              <input type="text" value={tienda} onChange={(e) => setTienda(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Horario</label>
-              <input type="text"/>
+              <input type="text" value={horario} onChange={(e) => setHorario(e.target.value)}/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Imagen</label> 
-              <input type="text"/> <SubirFoto/>
+              <input type="text" value={imagen} onChange={(e) => setImagen(e.target.value)}/> <SubirFoto/>
             </div>
             <div>
             <button type="submit">Registrarse</button>
