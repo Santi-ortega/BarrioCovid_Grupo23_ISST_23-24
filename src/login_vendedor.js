b37ba28eb36db6b125f26aa4b05625e2a62eccac
santi-ortega <santiof.12@gmail.com>
2024-04-05 12:40:00 +0200

    Comentarios login_vendedor

--- src/login_vendedor.js
+++ src/login_vendedor.js
@@ -4,26 +4,30 @@ import {Link} from "react-router-dom";
 import './login.css'
 import BackButton from './BackButton';
 
+//Esta es la página para que el vendedor se loguee
 function Login_vendedor() {
-  const [error, setError] = useState(false);
-  const [datosUsuario, setDatosUsuario] = useState({ correoElectronico: '', contrasena: '' }); //Almacen correo y contraseña
-  const navigate = useNavigate();
 
+  const [error, setError] = useState(false); //Parametro para informar de un posible error
+  const [datosUsuario, setDatosUsuario] = useState({ correoElectronico: '', contrasena: '' }); //Almacenamiento correo y contraseña
+  const navigate = useNavigate(); //Parámetro para navegar entre pantallas
   const handleClick = (id) => {
     navigate(`/login_vendedor/Inicio_vendedor`);
   }
+
+  //Renderizamos la página
     return (
       <div>
         <h1>Inicio de sesión</h1>
         <br></br>
+        {/*Creamos un form que enviará el vendedor para identificarse cada vez que quiera iniciar sesión*/}
         <form onSubmit={handleClick}>
           <div className="form-group">
             <label className='parametro_inicio_sesion' htmlFor="usuario">Usuario:</label>
-            <input type="text" id="usuario" />
+            <input type="text" id="usuario"/> {/*El vendedor introducirá el usuario con el que se registró*/}
           </div>
           <div className="form-group">
             <label className='parametro_inicio_sesion' htmlFor="contraseña">Contraseña:</label>
-            <input type="password" id="contraseña" />
+            <input type="password" id="contraseña" /> {/*El vendedor introducirá la contraseña con la que se registró*/}
           </div>
           <div className="form-group">
             <button type="submit">Aceptar</button>
