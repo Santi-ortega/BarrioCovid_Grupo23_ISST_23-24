213f063e30a4fb26c4b0a8617b39b44aec72a381
layunsk <alvarolayu@gmail.com>
2024-04-12 11:40:39 +0200

    registro y login vendedor

--- src/login_vendedor.js
+++ src/login_vendedor.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import './login.css'
 import BackButton from './BackButton';
 import './Inicio_comprador.css'
+import axios from 'axios';
 
 //Esta es la página para que el vendedor se loguee
 function Login_vendedor() {
@@ -11,9 +12,23 @@ function Login_vendedor() {
   const [error, setError] = useState(false); //Parametro para informar de un posible error
   const [datosUsuario, setDatosUsuario] = useState({ correoElectronico: '', contrasena: '' }); //Almacenamiento correo y contraseña
   const navigate = useNavigate(); //Parámetro para navegar entre pantallas
-  const handleClick = (id) => {
-    navigate(`/login_vendedor/Inicio_vendedor`);
-  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const response = await axios.post('http://localhost:8080/vendedor/login', datosUsuario);
+      if (response.status === 200) {
+        navigate('/login_vendedor/Inicio_vendedor');
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+      setError(true);
+    }
+  };
 
   //Renderizamos la página
     return (
@@ -21,18 +36,19 @@ function Login_vendedor() {
         <h1>Inicio de sesión</h1>
         <br></br>
         {/*Creamos un form que enviará el vendedor para identificarse cada vez que quiera iniciar sesión*/}
-        <form onSubmit={handleClick}>
+        <form onSubmit={handleSubmit}>
           <div className="store">
             <label className='parametro_inicio_sesion' htmlFor="usuario">Usuario:</label>
-            <input type="text" id="usuario"/> {/*El vendedor introducirá el usuario con el que se registró*/}
+            <input type="email" value={datosUsuario.correo} onChange={(event) => setDatosUsuario({ ...datosUsuario, correo: event.target.value })} required /> {/*El vendedor introducirá el usuario con el que se registró*/}
           </div>
           <div className="store">
             <label className='parametro_inicio_sesion' htmlFor="contraseña">Contraseña:</label>
-            <input type="password" id="contraseña" /> {/*El vendedor introducirá la contraseña con la que se registró*/}
+            <input type="password" value={datosUsuario.contraseña} onChange={(event) => setDatosUsuario({ ...datosUsuario, contraseña: event.target.value })} required /> {/*El vendedor introducirá la contraseña con la que se registró*/}
           </div>
           <div className="store">
             <button type="submit">Aceptar</button>
           </div>
+          {error && <p>Error al iniciar sesión. Verifica tus credenciales e intenta de nuevo.</p>}
         </form>
         <BackButton/>
       </div>
--- src/registro_vendedor.js
+++ src/registro_vendedor.js
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import {Link} from "react-router-dom";
 import './registros.css';
 import BackButton from './BackButton';
+import SubirFoto from './SubirFoto';
 import './Inicio_comprador.css'
+import axios from 'axios';
 
 //Pantalla para que los vendedores se registren
 function Registro_Vendedor() {
@@ -16,67 +18,92 @@ function Registro_Vendedor() {
   const [tienda, setTienda] = useState('');
   const [horario, setHorario] = useState('');
   const [imagen, setImagen] = useState('');
+  const [registroExitoso, setRegistroExitoso] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fileInputRef = useRef(null);
-
-  const handleImageChange = (e) => {
-    setImagen(e.target.files[0]);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const data = {
+      nombre,
+      apellido,
+      telefono,
+      correo,
+      contraseña,
+      direccion,
+      tienda,
+      horario,
+      imagen
+    };
 
-  if (!imagen) {
-      alert('Please select an image');
-      return;
-  }
+    try {
+      const response = await axios.post('http://localhost:8080/vendedor/registro', data)
 
-  const formData = new FormData();
-    formData.append("image", fileInputRef.current.files[0]);
-
-    // Send the formData to the server using fetch or axios
+      if (response.status === 200) {
+        // Registro exitoso
+        setRegistroExitoso(true);
+      } else {
+        // Manejar error en caso de fallo en el registro
+        setError('Error al registrar el comprador');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+      setError('Error de red: No se pudo conectar con el servidor');
+    }
   };
-  
+
+  if (registroExitoso) {
+    return (
+      <div>
+        <h2 className='registro_vendedor'>Registro de Vendedor</h2>
+        <p>¡Registro exitoso! Serás redirigido en unos momentos...</p>
+        {/* Redirigir al usuario después de un tiempo */}
+        {setTimeout(() => {
+          window.location.href = '/'; // Redirigir al usuario después de 2 segundos
+        }, 2000)}
+        <BackButton />
+      </div>
+    );
+  }
     return (
         <div>
           <h2>Registro de Vendedor</h2>
           {/*Creamos un form para que el vendedor rellene y guardemos en la base de datos*/}
-          <form onSubmit={handleSubmit} className='form'>
+          <form className='form' onSubmit={handleSubmit}>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Nombre</label>
-              <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+              <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Apellidos</label>
-              <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
+              <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Teléfono</label>
-              <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)}/>
+              <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Correo</label>
-              <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)}/>
+              <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)} required/>
             </div>            
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Contraseña</label>
-              <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)}/>
+              <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Dirección</label>
-              <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)}/>
+              <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Tienda</label>
-              <input type="text" value={tienda} onChange={(e) => setTienda(e.target.value)}/>
+              <input type="text" value={tienda} onChange={(e) => setTienda(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Horario</label>
-              <input type="text" value={horario} onChange={(e) => setHorario(e.target.value)}/>
+              <input type="text" value={horario} onChange={(e) => setHorario(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Imagen</label> 
-              <input type="file" accept=".jpg,.jpeg,.png" ref={fileInputRef} onChange={handleImageChange}/>
+              <input type="text" value={imagen} onChange={(e) => setImagen(e.target.value)} required/> <SubirFoto/>
             </div>
             <div>
             <button type="submit">Registrarse</button>
