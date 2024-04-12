92eaa6f0497987ff30c40e8e34b06f8ac5039047
asantorum <alvarosantorum@gmail.com>
2024-04-12 10:57:33 +0200

    back del comprador

--- src/registro_comprador.js
+++ src/registro_comprador.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import './registros.css';
 import BackButton from './BackButton';
 import './Inicio_comprador.css'
+import axios from 'axios';
 
 //Pantalla para que los compradores se registren
 function Registro_Comprador() {
@@ -14,39 +15,83 @@ function Registro_Comprador() {
   const [contraseña, setContraseña] = useState('');
   const [direccion, setDireccion] = useState('');
   const [isVulnerable, setIsVulnerable] = useState(false);
+  const [registroExitoso, setRegistroExitoso] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault ();
+    const data = {
+      nombre,
+      apellido,
+      telefono,
+      correo,
+      contraseña,
+      direccion,
+      isVulnerable
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8080/comprador/registro', data)
+      if (response.status === 200) {
+        // Registro exitoso
+        setRegistroExitoso(true);
+      } else {
+        // Manejar error en caso de fallo en el registro
+        setError('Error al registrar el comprador');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+      setError('Error de red: No se pudo conectar con el servidor');
+    }
+  };
+
+  if (registroExitoso) {
+    return (
+      <div>
+        <h2 className='registro_comprador'>Registro de Comprador</h2>
+        <p>¡Registro exitoso! Serás redirigido en unos momentos...</p>
+        {/* Redirigir al usuario después de un tiempo */}
+        {setTimeout(() => {
+          window.location.href = '/'; // Redirigir al usuario después de 2 segundos
+        }, 2000)}
+        <BackButton />
+      </div>
+    );
+  }
   
     return (
         <div>
           <h2 className='registro_comprador'>Registro de Comprador</h2>
           {/*Creamos un form para que el comprador rellene y guardemos en la base de datos*/}
-          <form className='form'>
+          <form className='form' onSubmit={handleSubmit}>
             <div className='apartado'>
               <li className='parametro_registro_comprador'>Nombre</li>
-              <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+              <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Apellido</label>
-              <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
+              <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Teléfono</label>
-              <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)}/>
+              <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Correo</label>
-              <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)}/>
+              <input type="text" value={correo} onChange={(e) => setCorreo(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Contraseña</label>
-              <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)}/>
+              <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)} required/>
             </div>
             <div className='apartado'>
               <label className='parametro_registro_comprador'>Dirección</label>
-              <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)}/>
+              <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)} required/>
             </div>
             <div className='store'>
               <label className='parametro_registro_comprador'>Soy Vulnerable</label>
-              <input type="checkbox" value={isVulnerable} onChange={(e) => setIsVulnerable(!isVulnerable)}/>
+              <input type="checkbox" value={isVulnerable} onChange={(e) => setIsVulnerable(!isVulnerable)} required/>
             </div>
             <div>
             <button type="submit">Registrarse</button>
