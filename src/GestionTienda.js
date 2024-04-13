a925907f4f901571c9c96ef325aefc808d3dfa39
santi-ortega <santiof.12@gmail.com>
2024-04-13 13:40:29 +0200

    Gestion productos

--- src/GestionTienda.js
+++ src/GestionTienda.js
@@ -8,86 +8,129 @@ import {Link} from "react-router-dom";
 import AñadirProducto from './AñadirProducto';
 
 function GestionTienda() {
-        const [products, setProducts] = useState([
-        { id: 1, idTienda: 1 , nombre: 'Lomo', description: 'Lomo ibérico', foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF08GU6Mf9Kz6m5rFMG6gATiYKbTqQLO3sOP0PztQ5QQ&s", precio: '10'},
-        { id: 2, idTienda: 1 , nombre: 'Chorizo', description: 'Chorizo de cantimpalo', foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbgSTjY9gplXLJO4s6V8YOQemL9mv3Vl4iAu9vR_4EMg&s", precio: '5'},
-        { id: 3, idTienda: 1 , nombre: 'Fuet', description: 'Fuet CasaTarradellas', foto: "https://images.openfoodfacts.org/images/products/848/000/055/1085/front_en.61.400.jpg", precio: '6'},
-        ]);
-const [editedProduct, setEditedProduct] = useState(null);
-const { idTienda } = useParams();
-
-const handleAddProduct = (newProduct) => {
-    // Agregar el nuevo producto a la lista de productos
-    setProducts([...products, newProduct]);
-    // Podrías enviar una solicitud POST a tu servidor aquí para guardar el nuevo producto en la base de datos
-  };
-
-const handleEditProduct = (id) => {
-            // Buscar el producto que se desea editar
-            const productToEdit = products.find(producto => producto.id === id);
-            // Establecer el producto a editar en el estado
-            setEditedProduct(productToEdit);
-          };
-        
-const handleUpdateProduct = () => {
-            // Actualizar el producto editado
-const updatedProducts = products.map(producto => {
-              if (producto.id === editedProduct.id) {
-                return editedProduct; // Usar el producto editado
-              }
-              return producto; // Mantener los otros productos sin editar
-            });
-            // Actualizar el estado de los productos
-            setProducts(updatedProducts);
-            // Limpiar el estado del producto editado
-            setEditedProduct(null);
-          };
-        
-const handleChange = (e) => {
-            const { name, value } = e.target;
-            // Actualizar el estado del producto editado
-            setEditedProduct({ ...editedProduct, [name]: value });
-          };  
-    
-const handleDeleteProduct = (id) => {
-    // Eliminar el producto con el id proporcionado
-    const updatedProducts = products.filter(producto => producto.id !== id);
-    setProducts(updatedProducts);
-  };
-
-  return (
-    <div>
-      <h1>{tiendasData.find(t => t.id === parseInt(idTienda))?.nombre}</h1>
-      <h2>Productos:</h2>
-      <ul className='producto'>
-        {products.map((producto) => (
-          <li key={producto.id} className='producto' >
-            {editedProduct && editedProduct.id === producto.id ? (
+        const [products, setProducts] = useState([]);
+        const [editedProduct, setEditedProduct] = useState(null);
+        const { idTienda } = useParams();
+        const [newProduct, setNewProduct] = useState({
+          nombre: '',
+          descripcion: '',
+          foto: '',
+          precio: '',
+          stock: ''
+        });
+      
+        const handleInputChange = (e) => {
+          const { name, value } = e.target;
+          setNewProduct({ ...newProduct, [name]: value });
+        };
+      
+        const handleAddProduct = async (e) => {
+          e.preventDefault();
+          try {
+            const response = await axios.post('http://localhost:8080/productos', newProduct);
+            if (response.status === 201) {
+              // Vuelve a cargar la lista de productos desde el servidor
+              const updatedProducts = await axios.get('http://localhost:8080/productos');
+              setProducts(updatedProducts.data);
+              // Reinicia el formulario
+              setNewProduct({
+                nombre: '',
+                descripcion: '',
+                foto: '',
+                precio: '',
+                stock: ''
+              });
+            } else {
+              console.error('Error al guardar el producto');
+            }
+          } catch (error) {
+            console.error('Error de red:', error);
+          }
+        };
+      
+        const handleEditProduct = (id) => {
+          // Buscar el producto que se desea editar
+          const productToEdit = products.find(producto => producto.id === id);
+          // Establecer el producto a editar en el estado
+          setEditedProduct(productToEdit);
+        };
+      
+        const handleUpdateProduct = () => {
+          // Actualizar el producto editado
+          const updatedProducts = products.map(producto => {
+            if (producto.id === editedProduct.id) {
+              return editedProduct; // Usar el producto editado
+            }
+            return producto; // Mantener los otros productos sin editar
+          });
+          // Actualizar el estado de los productos
+          setProducts(updatedProducts);
+          // Limpiar el estado del producto editado
+          setEditedProduct(null);
+        };
+      
+        const handleChange = (e) => {
+          const { name, value } = e.target;
+          // Actualizar el estado del producto editado
+          setEditedProduct({ ...editedProduct, [name]: value });
+        };
+        const handleFileChange = (e) => {
+          const file = e.target.files[0]; // Obtiene el primer archivo seleccionado
+          // Puedes hacer lo que necesites con el archivo aquí, como almacenarlo en el estado
+          setNewProduct({ ...newProduct, foto: file });
+        };
+        const handleDeleteProduct = (id) => {
+          // Eliminar el producto con el id proporcionado
+          const updatedProducts = products.filter(producto => producto.id !== id);
+          setProducts(updatedProducts);
+        };
+      
+        return (
+          <div>
+            <h1>{tiendasData.find(t => t.id === parseInt(idTienda))?.nombre}</h1>
+            <h2>Productos:</h2>
+            <ul className='producto'>
+            {products.map(producto => (
+              <li key={producto.id}>
               <div>
-                <input type="text" name="nombre" value={editedProduct.nombre} onChange={handleChange} />
-                <input type="text" name="description" value={editedProduct.description} onChange={handleChange} />
-                <input type="text" name="precio" value={editedProduct.precio} onChange={handleChange} />
-                <button onClick={handleUpdateProduct}>Guardar</button>
-              </div>
-            ) : (
-              <div>
-                <h3>{producto.nombre}</h3>
                 <img src={producto.foto} alt={producto.nombre} />
-                <p>{producto.description}</p>
+                <h3>{producto.nombre}</h3>
+                <p>{producto.descripcion}</p>
                 <p>Precio: {producto.precio}€</p>
+                <p>Stock: {producto.stock}</p>
                 <button onClick={() => handleEditProduct(producto.id)}>Editar</button>
                 <button onClick={() => handleDeleteProduct(producto.id)}>Eliminar</button>
-                
               </div>
-            )}
-            
-          </li>
-        ))}
-        <AñadirProducto onAddProduct={handleAddProduct} />
-      </ul>
+              </li>
+               ))}
+            </ul>
+            {/* Formulario para añadir un nuevo producto */}
+            <form onSubmit={handleAddProduct}>
+              <div>
+                <label htmlFor="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" value={newProduct.nombre} onChange={handleInputChange} required />
+              </div>
+              <div>
+                <label htmlFor="description">Descripción:</label>
+                <input type="text" id="description" name="description" value={newProduct.description} onChange={handleInputChange} required />
+              </div>
+              <div>
+                <label htmlFor="foto">Foto:</label>
+                 {/* Utilizamos un input de tipo file para permitir la carga de la foto desde el ordenador */}
+                <input type="file" id="foto" name="foto" onChange={handleFileChange} accept="image/*" required />
+              </div>
+              <div>
+                <label htmlFor="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" value={newProduct.precio} onChange={handleInputChange} required />
+              </div>
+              <div>
+                <label htmlFor="stock">Stock:</label>
+                <input type="number" id="stock" name="stock" value={newProduct.stock} onChange={handleInputChange} required />
+              </div>
+              <button type="submit">Añadir Producto</button>
+            </form>
+          </div>
+        );
+      }
       
-    </div>
-  );
-}
-
-export default GestionTienda;
+      export default GestionTienda;
