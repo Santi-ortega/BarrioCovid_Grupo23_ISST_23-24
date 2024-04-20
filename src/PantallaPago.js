d8c79b40620ce121026f993a66011e6ef1bc9eae
santi-ortega <santiof.12@gmail.com>
2024-04-20 21:50:41 +0200

    Preparación frontend pantalla de pago para mandar el pedido al back

--- src/PantallaPago.js
+++ src/PantallaPago.js
@@ -1,64 +1,92 @@
-import React, { useState } from 'react';
-import { Carrito } from './Carrito.js';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
 import './PantallaPago.css';
 
 //Esta es la pantalla de pago del comprasor
 function PantallaPago() {
 
-  const [nombre, setNombre] = useState('');
-  const [numeroTarjeta, setNumeroTarjeta] = useState('');
-  const [cvv, setCvv] = useState('');
-  const [hour, setHour] = useState({});
+  const [hour, setHour] = useState(0);
+  const [comprador, setComprador] = useState({});
+  const location = useLocation();
+  const { idVendedor, idComprador } = useParams();
+  const cartData = location.state?.cartData || [];
 
-  //Funcion para controlar el nombre que utiliza el comprador en su tarjeta
-  const handleNombreChange = (e) => {
-    setNombre(e.target.value);
-  };
+  useEffect(() => {
+    // Función para obtener el objeto del comprador
+    const fetchComprador = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/comprador/${idComprador}`);
+        setComprador(response.data);
+      } catch (error) {
+        console.error('Error al obtener el comprador:', error);
+      }
+    };
 
-  //Función para controlar el número de tarjeta que usa el comprador
-  const handleNumeroTarjetaChange = (e) => {
-    setNumeroTarjeta(e.target.value);
-  };
-
-  //Función para controlar el CVV que usa el comprador
-  const handleCvvChange = (e) => {
-    setCvv(e.target.value);
-  };
+    fetchComprador();
+  }, [idComprador]);
 
   //Función para enviar el proceso de pago
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    try {
+      const descripcion = cartData.map(item => `${item.name}: ${item.quantity}`).join(', ');
+  
+      const data = {
+        vendedorId: idVendedor,
+        comprador,
+        horaRecogida: hour,
+        descripcion: descripcion
+      };
+  
+      console.log(data); // Imprime los datos antes de serializarlos
+  
+      const jsonData = JSON.stringify(data); // Serializar el objeto data a JSON
+  
+      const response = await axios.post('http://localhost:8080/pedidos', jsonData, {
+        headers: {
+          'Content-Type': 'application/json' // Especificar el tipo de contenido como JSON
+        }
+      });
+  
+      if (response.status === 201) {
+        console.log('Pedido enviado con éxito');
+        // Aquí podrías redirigir a una página de confirmación o realizar otras acciones
+      } else {
+        console.error('Error al enviar el pedido');
+        // Manejar el error en caso de que la solicitud no sea exitosa
+      }
+    } catch (error) {
+      console.error('Error al enviar el pedido:', error);
+      // Manejar errores de red u otros errores
+    }
   };
 
   return (
     <div>
-      <h2>Pago con Tarjeta</h2>
-      {/*Introducimos un form para adquirir los datos de pago del comprador*/}
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="nombre">Nombre del Titular:</label>
-          <input type="text" id="nombre" value={nombre} onChange={handleNombreChange} /> {/*El comprador introducirá el nombre del titular de la tarjeta*/}
-        </div>
-        <div>
-          <label htmlFor="numeroTarjeta">Número de Tarjeta:</label>
-          <input type="text" id="numeroTarjeta" value={numeroTarjeta} onChange={handleNumeroTarjetaChange} /> {/*El comprador introducirá el numero de la tarjeta*/}
-        </div>
-        <div>
-          <label htmlFor="cvv">CVV:</label>
-          <input type="text" id="cvv" value={cvv} onChange={handleCvvChange} /> {/*El comprador introducirá el CVV de la tarjeta*/}
-        </div>
+        {/* Formulario para los detalles del pago */}
+        <div style={{ padding: '20px', backgroundColor: '#f8f9fa' }}>
+        <h2 style={{ marginBottom: '20px' }}>Resumen del pedido</h2>
+        <ul style={{ listStyleType: 'none', padding: '0' }}>
+        {cartData.map((item, index) => (
+        <li key={index} style={{ marginBottom: '10px' }}>
+        <span style={{ fontWeight: 'bold' }}>{item.name}</span>: {item.quantity}
+        </li>
+        ))}
+        </ul>
+      </div>
         <div>
           <label>Hora Recogida:</label>
-        <select value={hour} onChange={(e) => setHour(parseInt(e.target.value))}> {/*El comprador introducirá la hora de recogida deseada*/}
-  {[...Array(24).keys()].map((hour) => (
-    <option key={hour + 1} value={hour + 1}>
-      {hour + 1}:00
-    </option>
-  ))}
-</select>
-</div>
-        <button type="submit">Pagar</button>
+          <select value={hour} onChange={(e) => setHour(parseInt(e.target.value))}>
+            {[...Array(24).keys()].map((hour) => (
+              <option key={hour + 1} value={hour + 1}>
+                {hour + 1}:00
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Aceptar</button>
       </form>
     </div>
   );
